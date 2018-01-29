86cdee88a8c65aac87fe86fe9979bd89dd12c670
kmcpeek <kevin.j.mcpeek@Live.com>
2018-01-28 19:57:29 -0600

    Added Copyright notice to source files

--- calculator.js
+++ calculator.js
@@ -1,3 +1,21 @@
+/***********************************************************************************
+ Jacob Midgley's Game Website - contains JS/Angular web-based games and utilities
+ Copyright (C) 2018 Jacob Midgley and Kevin McPeek
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/
+ **********************************************************************************/
+
 var FKeyPad = document.Keypad;
 var Accumulate = 0;
 var FlagNewNum = false;
--- coin-tap-game/coin-tap-game.js
+++ coin-tap-game/coin-tap-game.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Modified by Jacob Midgley March 30, 2017
+
 (function() {
 	// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 	// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
